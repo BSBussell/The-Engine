00f4a081a9da5a0047e59c9612cf6f4192626035
BSBussell <benjaminsbussell@gmail.com>
2018-02-10 21:56:36 -0600

    Hello World
    The First Commit

---
+++ Engine/Game.cpp
@@ -0,0 +1,9 @@
+//
+//  Game.cpp
+//  Engine
+//
+//  Created by Benjamin Bussell on 2/10/18.
+//  Copyright © 2018 Benjamin Bussell. All rights reserved.
+//
+
+#include "Game.hpp"
---
+++ Engine/Game.hpp
@@ -0,0 +1,37 @@
+//
+//  Game.hpp
+//  Engine
+//
+//  Created by Benjamin Bussell on 2/10/18.
+//  Copyright © 2018 Benjamin Bussell. All rights reserved.
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+class Engine {
+	
+public:
+	Engine();
+	~Engine();
+	
+	void init(const char* title, int xPos, int yPos, int width, int height, bool fullScreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	
+	bool running();
+	
+private:
+	
+	bool isRunning;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	
+};
+
+#endif /* Game_hpp */
